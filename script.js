3901776cd223981d79cd0162d94f31d057dd91a8
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-08-08 20:36:47 +0200

    display list of Disney Characters

--- script.js
+++ script.js
@@ -16,4 +16,39 @@ function fetchCharacters() {
     });
 }
 
+function displayList(characters) {
+  const filmCharacters = characters.filter(
+    (character) => character.films.length
+  );
+  console.log("film character", filmCharacters);
+
+  filmCharacters.forEach((character) => {
+    const singleCharacter = document.createElement("div");
+    singleCharacter.className = "singleCharacter";
+    charactersContainer.append(singleCharacter);
+
+    const characterImg = document.createElement("img");
+    characterImg.src = `${character.imageUrl}`;
+
+    const characterName = document.createElement("p");
+    characterName.innerText = `${character.name}`;
+
+    const filmsCount = document.createElement("p");
+    filmsCount.innerText = `${character.films.length}`;
+
+    const addToFavoritesBtn = document.createElement("button");
+    addToFavoritesBtn.innerText = "Dodaj do ulubionych";
+    addToFavoritesBtn.addEventListener("click", () =>
+      addToFavorites(character)
+    );
+
+    singleCharacter.append(
+      characterImg,
+      characterName,
+      filmsCount,
+      addToFavoritesBtn
+    );
+  });
+}
+
 fetchCharacters();
