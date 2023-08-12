3a40c2afeb35d4a982f0d1eeb3863ad752e6ab65
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-08-12 18:49:10 +0200

    add cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,35 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("Testing Disney Character Page", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5501/index.html");
+  });
+
+  it("displays the Most Popular section", () => {
+    cy.get(".mostPopularCharacters").should("be.visible");
+  });
+
+  it("displays 3 most popular Characters", () => {
+    cy.get(".popularCharacter").should("have.length", 3);
+  });
+
+  it("displays the Search section", () => {
+    cy.get(".searchSectionContainer").should("be.visible");
+  });
+
+  it("searches character", () => {
+    cy.on("window:before:unload", (event) => {
+      event.preventDefault();
+    });
+    cy.get("input").type("Baloo").type("{enter}");
+    cy.get(".singleCharacter")
+      .filter(':contains("Baloo")')
+      .should("have.length", 1);
+    cy.get(".nameContainer p").should("contain", "Baloo");
+  });
+
+  it("adds and removes from favorites", () => {
+    cy.get("#star112").click();
+    cy.get(".favoritesContainer #star112").should("be.visible");
+    cy.get(".favoritesContainer #star112").click();
+    cy.get(".favoritesContainer .singleCharacter").should("not.exist");
   });
 });
